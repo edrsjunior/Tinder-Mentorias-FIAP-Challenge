e224bff72df596a5df5559cd9eb694fde71aafe7
Washington Matias <86788796+WashingtonMatias@users.noreply.github.com>
2024-05-20 20:54:50 -0300

    Add scroll

--- app/src/main/java/com/example/tinderfiap_challenge/cadastro/SignUpScreen.kt
+++ app/src/main/java/com/example/tinderfiap_challenge/cadastro/SignUpScreen.kt
@@ -2,7 +2,9 @@ package com.example.tinderfiap_challenge.cadastro
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -22,126 +24,130 @@ fun SignUpScreen(onNavigateSignUpDone: () -> Unit) {
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF222E36))
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(16.dp)
     ) {
-        Text(
-            text = "Vamos conhecer você um pouco mais...",
-            fontSize = 48.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
-
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Seu nome é") },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(40.dp), // Add border radius
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White
-            ),
-            textStyle = LocalTextStyle.current.copy(color = Color.White)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = { Text("Seu sobrenome") },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(40.dp), // Add border radius
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White
-            ),
-            textStyle = LocalTextStyle.current.copy(color = Color.White)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Seu melhor email") },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(40.dp), // Add border radius
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White
-            ),
-            textStyle = LocalTextStyle.current.copy(color = Color.White)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = zipCode,
-            onValueChange = { zipCode = it },
-            label = { Text("Qual seu CEP?") },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(40.dp), // Add border radius
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White
-            ),
-            textStyle = LocalTextStyle.current.copy(color = Color.White)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Senha") },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(40.dp), // Add border radius
-            visualTransformation = PasswordVisualTransformation(),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White
-            ),
-            textStyle = LocalTextStyle.current.copy(color = Color.White)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = { Text("Confirmação Senha") },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(40.dp), // Add border radius
-            visualTransformation = PasswordVisualTransformation(),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White
-            ),
-            textStyle = LocalTextStyle.current.copy(color = Color.White)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = { onNavigateSignUpDone() },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
-            Text("Continuar", color = Color.Black)
+            Text(
+                text = "Vamos conhecer você um pouco mais...",
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Seu nome é") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp), // Add border radius
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.White
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = { lastName = it },
+                label = { Text("Seu sobrenome") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp), // Add border radius
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.White
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Seu melhor email") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp), // Add border radius
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.White
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = zipCode,
+                onValueChange = { zipCode = it },
+                label = { Text("Qual seu CEP?") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp), // Add border radius
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.White
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Senha") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp), // Add border radius
+                visualTransformation = PasswordVisualTransformation(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.White
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = { Text("Confirmação Senha") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp), // Add border radius
+                visualTransformation = PasswordVisualTransformation(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.White
+                ),
+                textStyle = LocalTextStyle.current.copy(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { onNavigateSignUpDone() },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+            ) {
+                Text("Continuar", color = Color.Black)
+            }
         }
     }
 }
